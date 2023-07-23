4f1ece4fe92365c9952afa8ad9865193f05c0e95
Yovanni Pedroza <yovannipedroza@gmail.com>
2023-07-22 22:06:24 -0500

    Se crea branch 2

--- listaDeContactos.js
+++ listaDeContactos.js
@@ -8,12 +8,16 @@ let listaDeContactos=[
         Edad: 19,
         Celular: 300300300,
         Email: "valentina@gmail.com",
+        Ubicación: {
+            Ciudad: "Tunja",
+            Dirección: "Calle 130 Carrera 20",
+        }
     }
         ]
 
 // Se crea la función Agregar
 
-    function AgregarContactos (id,Nombre,Apellido,Edad,Celular,Email){
+    function AgregarContactos (id,Nombre,Apellido,Edad,Celular,Email,Ciudad, Dirección){
         let nuevo = {
             id:id,
             Nombre: Nombre,
@@ -21,6 +25,8 @@ let listaDeContactos=[
             Edad: Edad,
             Celular: Celular,
             Email: Email,
+            Ciudad: Ciudad,
+            Dirección: Dirección,
         }
          listaDeContactos.push(nuevo);
          console.log (listaDeContactos);    
@@ -29,9 +35,9 @@ let listaDeContactos=[
 
 // Se agrega contactos
 
-    AgregarContactos(1,"Yovanni","Pedroza",44,300300400,"yovanni@gmail.com");
-    AgregarContactos(2,"Nilsa","Torres",42,300300500,"nilsa@gmail.com");
-    AgregarContactos(3,"Yovanni Matias","Pedroza",9,300300600,"yovannimatias@gmail.com");
+    AgregarContactos(1,"Yovanni","Pedroza",44,300300400,"yovanni@gmail.com","Pereira",">Calle 130 con 13");
+    AgregarContactos(2,"Nilsa","Torres",42,300300500,"nilsa@gmail.com","Cali", "Carrera 5 calle 12");
+    AgregarContactos(3,"Yovanni Matias","Pedroza",9,300300600,"yovannimatias@gmail.com", "Bogotá", "Transversal 16 calle 30");
 
     // Se crea funcion de eleminar
 
